b9e9b5580885d8c1174c085c876e523219d11f8d
davidwhogg <david.hogg@nyu.edu>
2014-12-10 09:09:54 -0600

    make figs

--- code/sidereal_day.py
+++ code/sidereal_day.py
@@ -4,7 +4,9 @@ import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
 
-def download_data():
+DEFAULT_HW = 128 # for image statistics section
+
+def _download_data():
     urlbase = "http://telescope.livjm.ac.uk/data/archive/data/lt/Skycam/"
     list = [
         ("2013/20130614/", "a_e_20130614_114_1_1_1.fits.gz"),
@@ -33,30 +35,34 @@ def _normalize(vec):
 def _get_time(str):
     return datetime.strptime(str, "%Y-%m-%dT%H:%M:%S")
 
-def cross_correlate_files(fn1, fn2, hw=128):
+def _get_section_and_time(hdulist, hw):
+    x1 = hdulist1[0].header["NAXIS1"] / 2 - hw
+    x2 = x1 + hw + hw
+    y1 = hdulist1[0].header["NAXIS2"] / 2 - hw
+    y2 = y1 + hw + hw
+    vec = _normalize(hdulist1[0].data[y1:y2, x1:x2])
+    time = _get_time(hdulist1[0].header["DATE-OBS"])
+    return vec, time
+
+def cross_correlate_files(fn1, fn2, hw=DEFAULT_HW):
     print fn1, fn2
     hdulist1 = fits.open(fn1)
     hdulist2 = fits.open(fn2)
     if (hdulist1[0].header["NAXIS1"] != hdulist2[0].header["NAXIS1"]) or (hdulist1[0].header["NAXIS2"] != hdulist2[0].header["NAXIS2"]):
         print hdulist1[0].header["NAXIS1"], hdulist2[0].header["NAXIS1"], hdulist1[0].header["NAXIS2"], hdulist2[0].header["NAXIS2"]
         assert False
-    x1 = hdulist1[0].header["NAXIS1"] / 2 - hw
-    x2 = x1 + hw + hw
-    y1 = hdulist1[0].header["NAXIS2"] / 2 - hw
-    y2 = y1 + hw + hw
-    vec1 = _normalize(hdulist1[0].data[y1:y2, x1:x2])
-    vec2 = _normalize(hdulist2[0].data[y1:y2, x1:x2])
-    t1 = _get_time(hdulist1[0].header["DATE-OBS"])
-    t2 = _get_time(hdulist2[0].header["DATE-OBS"])
+    vec1, t1 = _get_section_and_time(hdulist1, hw)
+    vec2, t2 = _get_section_and_time(hdulist2, hw)
     cc = np.sum(vec1 * vec2)
-    return t1, t2, cc
+    dt = _get_time(t2) - _get_time(t1)
+    return dt, cc
 
 def compare(list, jj, prefix, truth):
     dts = []
     ccs = []
     for ii in np.arange(jj + 1, len(list)):
-        t1, t2, cc = cross_correlate_files(list[jj][1], list[ii][1])
-        dts.append((t2 - t1).total_seconds())
+        dt, cc = cross_correlate_files(list[jj][1], list[ii][1])
+        dts.append(dt.total_seconds())
         ccs.append(cc)
     dts = np.array(dts)
     ccs = np.array(ccs)
@@ -72,8 +78,19 @@ def compare(list, jj, prefix, truth):
     plt.xlim(truth - 4., truth + 4.)
     plt.ylim(0.95 * np.max(ccplot), 1.05 * np.max(ccplot))
     plt.savefig("%s_zoom.png" % prefix)
+    return None
+
+def make_plot(fn, prefix):
+    hdulist = fits.open(fn)
+    time, vec = _get_section_and_time(hdulist, DEFAULT_HW)
+    plt.clf()
+    plt.imshow(vec, cmap="grey", interpolate="nearest")
+    plt.savefig(prefix + ".jpg")
+    return None
 
 if __name__ == "__main__":
-    list = download_data()
+    list = _download_data()
+    for foo, fn in list:
+        make_plot(fn, fn)
     compare(list, 1, "sd", 86164.0905)
-    compare(list, 0, "sy", 365. * 86164.0905)
+    # compare(list, 0, "sy", 365. * 86164.0905)
